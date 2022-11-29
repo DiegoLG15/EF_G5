3212b5e34f55d5cf718e9ecf3a2d3aaf3bcafe3d
Gil Zanabria <gzanabriaz28@gmail.com>
2022-11-29 13:04:29 -0500

    beans de servicios y tiposervicios

---
+++ EF_G5/src/main/java/com/example/ef_g5/Beans/Servicios.java
@@ -0,0 +1,142 @@
+package com.example.ef_g5.Beans;
+
+public class Servicios {
+
+    private int idServicios;
+    private TipoServicios tipoServicios;
+    private Usuario usuario;
+    private String densidad;
+    private String grosor;
+    private String altura;
+    private String material;
+    private String url;
+    private String autoservicio;
+    private String color;
+    private String ancho;
+    private String largo;
+    private String resolucion;
+    private boolean borradoLogico;
+    private String fecha;
+
+
+    public int getIdServicios() {
+        return idServicios;
+    }
+
+    public void setIdServicios(int idServicios) {
+        this.idServicios = idServicios;
+    }
+
+    public TipoServicios getTipoServicios() {
+        return tipoServicios;
+    }
+
+    public void setTipoServicios(TipoServicios tipoServicios) {
+        this.tipoServicios = tipoServicios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getDensidad() {
+        return densidad;
+    }
+
+    public void setDensidad(String densidad) {
+        this.densidad = densidad;
+    }
+
+    public String getGrosor() {
+        return grosor;
+    }
+
+    public void setGrosor(String grosor) {
+        this.grosor = grosor;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAutoservicio() {
+        return autoservicio;
+    }
+
+    public void setAutoservicio(String autoservicio) {
+        this.autoservicio = autoservicio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(String ancho) {
+        this.ancho = ancho;
+    }
+
+    public String getLargo() {
+        return largo;
+    }
+
+    public void setLargo(String largo) {
+        this.largo = largo;
+    }
+
+    public String getResolucion() {
+        return resolucion;
+    }
+
+    public void setResolucion(String resolucion) {
+        this.resolucion = resolucion;
+    }
+
+    public boolean isBorradoLogico() {
+        return borradoLogico;
+    }
+
+    public void setBorradoLogico(boolean borradoLogico) {
+        this.borradoLogico = borradoLogico;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+}
---
+++ EF_G5/src/main/java/com/example/ef_g5/Beans/TipoServicios.java
@@ -0,0 +1,22 @@
+package com.example.ef_g5.Beans;
+
+public class TipoServicios {
+    public int getIdTipoServicios() {
+        return idTipoServicios;
+    }
+
+    public void setIdTipoServicios(int idTipoServicios) {
+        this.idTipoServicios = idTipoServicios;
+    }
+
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    private int idTipoServicios;
+    private String nombreServicio;
+}

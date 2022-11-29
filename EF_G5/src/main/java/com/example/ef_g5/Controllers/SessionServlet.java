566bbd4d66f3f1711a7ada29a5efd7228977144a
DiegoLG15 <a20200839@pucp.edu.pe>
2022-11-29 13:07:21 -0500

    bens usuario y tipo Diego

--- EF_G5/src/main/java/com/example/ef_g5/Controllers/SessionServlet.java
+++ EF_G5/src/main/java/com/example/ef_g5/Controllers/SessionServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "Servlet", value = "/Servlet")
-public class ServletLogin extends HttpServlet {
+public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
